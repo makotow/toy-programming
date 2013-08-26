476081feb65d9e742eaf0d1c23d83fb47adf55d4
makotow <makoto.wata@gmail.com>
2013-08-27 01:17:47 +0900

    snapshot

---
+++ ruby/clean_test.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+# -*- mode: ruby; coding: utf-8 -*-
+
+require "clean2"
+require "test/unit"
+
+class CleanTest < Test::UnitTest::TestCase
+  def must "backup file(~) is deleted" 
+end
+end
